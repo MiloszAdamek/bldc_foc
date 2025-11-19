34ac9ca83b7c3fc8835f8fae7ab4717a940db341
Milosz <60300424+MiloszAdamek@users.noreply.github.com>
2025-11-19 14:01:41 +0100

    strojenie PI od predkosci

--- Projects/BLDC_FOC/Core/Inc/App/config.h
+++ Projects/BLDC_FOC/Core/Inc/App/config.h
@@ -62,8 +62,8 @@
 #define PI_KI_IQ 500.0f
 #define PI_LIMIT_IQ (VOLTAGE_SUPPLY / M_SQRT3)
 
-#define PI_KP_V 0.01f
-#define PI_KI_V 0.005f
+#define PI_KP_V 0.005f
+#define PI_KI_V 0.02f
 #define PI_LIMIT_V 3.0f
 
 #ifdef ENABLE_SERIAL_DEBUGGING
